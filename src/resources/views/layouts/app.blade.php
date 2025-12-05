1a26b9fd7fc95c377baf61cf1cbfb2f74953723a
iida hideaki <hideaki.19790727@gmail.com>
2025-12-05 18:20:15 +0900

    coachtech中級模擬案件2日目ver.1.0（すべてのブレードファイルの継承元（親テンプレート）作成）

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('css/layouts/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/layouts/common.css') }}">
+    @yield('css')
+</head>
+
+<body>
+    <header class="header">
+        <div class="header-inner">
+            <div class="header-logo">
+                <img src="{{ asset('images/logo.svg') }}" alt="coachtech" />
+            </div>
+            <div class="header-button">
+                @yield('button')
+            </div>
+        </div>
+    </header>
+    <main>
+        @yield('content')
+    </main>
+</body>
+
+</html>
