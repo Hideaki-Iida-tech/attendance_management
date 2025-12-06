5a950c95a7f63c945fe7749c1d603373c1bbac87
iida hideaki <hideaki.19790727@gmail.com>
2025-12-06 10:10:11 +0900

    coachtech中級模擬案件3日目ver.3.0（管理画面のメニュー部分のテンプレート作成）

---
+++ src/resources/views/layouts/admin-menu.blade.php
@@ -0,0 +1,13 @@
+@section('button')
+<div class="header-button">
+
+    <a href="" class="header-button-attendance-list">勤怠一覧</a>
+    <a href="" class="header-button-staff-list">スタッフ一覧</a>
+    <a href="" class="header-button-request-list">申請一覧</a>
+    <form action="/logout" method="post">
+        @csrf
+        <button class="header-button-logout">ログアウト</button>
+    </form>
+
+</div>
+@endsection
